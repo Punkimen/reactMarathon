e9c386876f9f534652b16d9186343782ca9ea90a
max <punkimen666@gmail.com>
2021-09-09 19:58:52 +0300

    homeWork-2 done

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header";
+import Layout from "./components/Layout";
+import Footer from "./components/Footer";
+import bg1 from "./assets/img/bg1.jpg"
+import bg3 from "./assets/img/bg3.jpg"
 
-function App() {
+
+const App = () => {
+  console.log(bg1);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World!
-        </p>
-      </header>
-    </div>
-  );
+    <>
+      <Header title="Pokemon" descr="Pokemon is fun" />
+      <Layout urlBg={bg1} title="section width BG" descr="image-test" />
+      <Layout colorBg="red " title="section width BG-color" descr="image-test" />
+      <Layout urlBg={bg3} title="section width BG" descr="image-test" />
+      <Footer />
+    </>
+  )
 }
-
 export default App;
